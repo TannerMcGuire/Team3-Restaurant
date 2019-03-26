00604cfab6870b9cc92966264b5ceb291ced47dc
Tanner McGuire <47872087+TannerMcGuire@users.noreply.github.com>
2019-03-26 17:29:06 -0400

    Add files via upload
    Base Version of Eclipse set-up

---
+++ Restaurant/userinterface/ViewFactory.java
@@ -0,0 +1,33 @@
+package userinterface;
+
+import impresario.IModel;
+
+//==============================================================================
+public class ViewFactory {
+
+	public static View createView(String viewName, IModel model) {
+		if (viewName.equals("LibrarianView") == true) {
+			return new LibrarianView(model);
+		} else if (viewName.equals("BookView") == true) {
+			return new BookView(model);
+		} else if (viewName.equals("PatronView") == true) {
+			return new PatronView(model);
+		} else if (viewName.equals("SearchBookView") == true) {
+			return new SearchBookView(model);
+		} else if (viewName.equals("SearchPatronView") == true) {
+			return new SearchPatronView(model);
+		} else if (viewName.equals("BookCollectionView") == true) {
+			return new BookCollectionView(model);
+		} else if (viewName.equals("PatronCollectionView") == true) {
+			return new PatronCollectionView(model);
+		} else
+			return null;
+	}
+
+	/*
+	 * public static Vector createVectorView(String viewName, IModel model) {
+	 * if(viewName.equals("SOME VIEW NAME") == true) { //return [A NEW VECTOR VIEW
+	 * OF THAT NAME TYPE] } else return null; }
+	 */
+
+}
