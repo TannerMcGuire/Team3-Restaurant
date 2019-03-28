c6d6da2e392ba10e4cbd55c2f797f9d556362093
Tanner McGuire <47872087+TannerMcGuire@users.noreply.github.com>
2019-03-28 18:01:50 -0400

    Tester for use

---
+++ Restaurant/tester/Tester.java
@@ -0,0 +1,98 @@
+// specify the package
+package tester;
+
+// system imports
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.io.FileOutputStream;
+import java.io.File;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+// project imports
+import event.Event;
+import event.EventLog;
+import common.PropertyFile;
+import model.InventoryManager;
+
+import userinterface.MainStageContainer;
+import userinterface.WindowPosition;
+
+
+/** The class containing the main program  for the ATM application */
+//==============================================================
+public class Tester extends Application
+{
+
+	private InventoryManager myManager;		// the main behavior for the application
+
+	/** Main frame of the application */
+	private Stage mainStage;
+
+
+	// start method for this class, the main application object
+	//----------------------------------------------------------
+	public void start(Stage primaryStage)
+	{
+	   System.out.println("Restaurant Version 0.1");
+	   System.out.println("Copyright Team 3 2019");
+
+           // Create the top-level container (main frame) and add contents to it.
+	   MainStageContainer.setStage(primaryStage, "Restaurant Inventory Version 0.1");
+	   mainStage = MainStageContainer.getInstance();
+
+	   // Finish setting up the stage (ENABLE THE GUI TO BE CLOSED USING THE TOP RIGHT
+	   // 'X' IN THE WINDOW), and show it.
+           mainStage.setOnCloseRequest(new EventHandler <javafx.stage.WindowEvent>() {
+            @Override
+            public void handle(javafx.stage.WindowEvent event) {
+                System.exit(0);
+            }
+           });
+
+           try
+	   {
+		myManager = new InventoryManager();
+	   }
+	   catch(Exception exc)
+	   {
+		System.err.println("Tester.Tester - could not create Manager!");
+		new Event(Event.getLeafLevelClassName(this), "Tester.<init>", "Unable to create Manager object", Event.ERROR);
+		exc.printStackTrace();
+	   }
+
+
+  	   WindowPosition.placeCenter(mainStage);
+
+           mainStage.show();
+	}
+
+
+	/** 
+	 * The "main" entry point for the application. Carries out actions to
+	 * set up the application
+	 */
+	//----------------------------------------------------------
+    	public static void main(String[] args)
+	{
+
+		launch(args);
+	}
+
+}
