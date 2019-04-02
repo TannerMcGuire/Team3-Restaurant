c873ecdf5a26e9986d3b31ce1ba47a9199c45cfe
dhanley1001 <dahanley101@gmail.com>
2019-04-02 16:54:08 -0400

    Updated Vendor

--- Restaurant/userinterface/VendorView.java
+++ Restaurant/userinterface/VendorView.java
@@ -74,7 +74,7 @@ public class VendorView extends View {
 		HBox container = new HBox();
 		container.setAlignment(Pos.CENTER);
 
-		Text titleText = new Text(" //Restaurant Name\\ ");
+		Text titleText = new Text(" Inventory Management ");
 		titleText.setFont(Font.font("Arial", FontWeight.BOLD, 20));
 		titleText.setWrappingWidth(300);
 		titleText.setTextAlignment(TextAlignment.CENTER);
